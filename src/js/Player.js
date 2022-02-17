5e8e69a425d6c09a0116e3adf6c41920f5957d7c
Mykola Kolisnyk <kolesnikny@gmail.com>
2022-02-17 12:14:55 +0200

    Initial commit

---
+++ src/js/Player.js
@@ -0,0 +1,26 @@
+class Player {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 200,
+        };
+        this.velocity = {
+            x: 0,
+            y: 0,
+        };
+
+        //this.image =
+        this.width = 100;
+        this.height = 100;
+    }
+
+    draw() {
+        paintContext.fillStyle = 'red';
+        paintContext.fillRect(
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        );
+    }
+}
